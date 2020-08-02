d97863f299f8f98a57e0b1cb6c1b9435f97092f1
Kandimalla Shivadeep Reddy <kandimallas@dbs.com>
2020-08-02 23:34:23 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'select the Location'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('You have selected the Country: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Country:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="Singapore">Singapore</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="Indonesia">Indonesia</option>
+            <option value="Thailand">Thailand</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
